700c2e60e8f5ad6cd6cf39f589426ba966bb1fe0
Mark <mark@>
2022-02-03 00:57:15 +0000

    feat: add new authorizor for admin-only and improve efficiency of process

--- backend/src/handlers/authorization.js
+++ backend/src/handlers/authorization.js
@@ -1,21 +1,45 @@
-const validateAuthToken = require("../application/validate-auth-token");
+const getUser = require("../persistence/get-user");
+const getAuthToken = require("../utility/get-auth-token");
 
-module.exports.authorize = async (event, context, callback) => {
-	const authorizeResult = await validateAuthToken(event);
+module.exports.authorizeUser = async (event, context, callback) => {
+	const authorizationToken = getAuthToken(event);
+	const userObj = await getUser(authorizationToken);
 
-	if (authorizeResult.success) {
-		return {
-			principalId: "user",
-			policyDocument: {
-				Version: "2012-10-17",
-				Statement: [{
-					Action: "execute-api:Invoke",
-					Effect: "Allow",
-					Resource: event.methodArn
-				}]
-			}
-		};
+	if (userObj) {
+		return getAllowPolicy(event.methodArn, userObj);
 	} else {
-		callback(`Unauthorized: ${authorizeResult.message}`, null);
+		callback("Unauthorized", null);
 	}
+};
+
+module.exports.authorizeAdmin = async (event, context, callback) => {
+	const authorizationToken = getAuthToken(event);
+	const userObj = await getUser(authorizationToken);
+
+	if (isUserAnAdmin(userObj)) {
+		return getAllowPolicy(event.methodArn, userObj);
+	} else {
+		callback("Unauthorized", null);
+	}
+};
+
+const isUserAnAdmin = userObj => userObj
+	&& userObj.Tags
+	&& userObj.Tags.includes("Admin");
+
+const getAllowPolicy = (methodArn, userObj) => {
+	return {
+		principalId: "user",
+		policyDocument: {
+			Version: "2012-10-17",
+			Statement: [{
+				Action: "execute-api:Invoke",
+				Effect: "Allow",
+				Resource: methodArn
+			}]
+		},
+		context: {
+			"user": userObj
+		}
+	};
 };
